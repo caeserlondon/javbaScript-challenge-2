b7b4b186e1c820ed57d8bff0cf1352274f4384ac
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-04-12 19:40:32 +0700

    last commit

--- index.js
+++ index.js
@@ -147,20 +147,20 @@
 //    z should turn to a
 //  ex. 'hello there' === 'Ifmmp UIfsf'
 
-function letterChange(str) {
-	let newStr = str.toLowerCase().replace(/[a-z]/gi, function (char) {
-		if (char === "z") {
-			return "a";
-		} else {
-			return String.fromCharCode(char.charCodeAt() + 1);
-		}
-	});
-	newStr = newStr.replace(/a|e|i|o|u/gi, function (vowel) {
-		return vowel.toUpperCase();
-	});
-	return newStr;
-}
+// function letterChange(str) {
+// 	let newStr = str.toLowerCase().replace(/[a-z]/gi, function (char) {
+// 		if (char === "z") {
+// 			return "a";
+// 		} else {
+// 			return String.fromCharCode(char.charCodeAt() + 1);
+// 		}
+// 	});
+// 	newStr = newStr.replace(/a|e|i|o|u/gi, function (vowel) {
+// 		return vowel.toUpperCase();
+// 	});
+// 	return newStr;
+// }
 
-const output = letterChange("hello there");
+// const output = letterChange("hello there");
 
-console.log(output);
+// console.log(output);
